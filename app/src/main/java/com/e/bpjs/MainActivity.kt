f7cf9632c674a4275c7dccd50963614531442949
Lina Handayani <lina.handayani06@gmail.com>
2019-12-04 15:20:33 +0700

    carousel

--- app/src/main/java/com/e/bpjs/MainActivity.kt
+++ app/src/main/java/com/e/bpjs/MainActivity.kt
@@ -2,11 +2,27 @@ package com.e.bpjs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.bumptech.glide.Glide
+import com.synnapps.carouselview.CarouselView
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var carouselView: CarouselView
+    val sampleImages = intArrayOf(
+        R.drawable.satu,
+        R.drawable.dua,
+        R.drawable.tiga
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        carouselView = findViewById(R.id.carouselView)
+        carouselView.setPageCount(3)
+//        posisi and image
+        carouselView.setImageListener { position, imageView ->
+            Glide.with(this@MainActivity).load(sampleImages[position]).into(imageView)
+        }
     }
+
+
 }
